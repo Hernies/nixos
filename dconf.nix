3e852b661d641306047e6036707d0ccbaf2e684a
Hernies <hernancalvoaguiar.2@gmail.com>
2024-07-04 23:16:59 +0200

    # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix { lib, ... }:
    with lib.hm.gvariant;
    
    {
      dconf.settings = {
        "org/gnome/Console" = {
          last-window-maximised = false;
          last-window-size = mkTuple [ 1364 480 ];
        };
    
        "org/gnome/control-center" = {
          last-panel = "display";
          window-state = mkTuple [ 980 640 false ];
        };
    
        "org/gnome/desktop/app-folders" = {
          folder-children = [ "Utilities" "YaST" "Pardus" ];
        };
    
        "org/gnome/desktop/app-folders/folders/Pardus" = {
          categories = [ "X-Pardus-Apps" ];
          name = "X-Pardus-Apps.directory";
          translate = true;
        };
    
        "org/gnome/desktop/app-folders/folders/Utilities" = {
          apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
          categories = [ "X-GNOME-Utilities" ];
          name = "X-GNOME-Utilities.directory";
          translate = true;
        };
    
        "org/gnome/desktop/app-folders/folders/YaST" = {
          categories = [ "X-SuSE-YaST" ];
          name = "suse-yast.directory";
          translate = true;
        };
    
        "org/gnome/desktop/input-sources" = {
          sources = [ (mkTuple [ "xkb" "us+intl" ]) ];
          xkb-options = [ "terminate:ctrl_alt_bksp" ];
        };
    
        "org/gnome/desktop/notifications" = {
          application-children = [ "gnome-power-panel" "org-gnome-console" ];
        };
    
        "org/gnome/desktop/notifications/application/gnome-power-panel" = {
          application-id = "gnome-power-panel.desktop";
        };
    
        "org/gnome/desktop/notifications/application/org-gnome-console" = {
          application-id = "org.gnome.Console.desktop";
        };
    
        "org/gnome/epiphany" = {
          ask-for-default = false;
        };
    
        "org/gnome/epiphany/state" = {
          is-maximized = false;
          window-size = mkTuple [ 1024 768 ];
        };
    
        "org/gnome/evolution-data-server" = {
          migrated = true;
        };
    
        "org/gnome/nautilus/preferences" = {
          default-folder-viewer = "icon-view";
          migrated-gtk-settings = true;
          search-filter-time-type = "last_modified";
        };
    
        "org/gnome/nautilus/window-state" = {
          initial-size = mkTuple [ 890 550 ];
        };
    
        "org/gnome/shell" = {
          disabled-extensions = [ "auto-move-windows@gnome-shell-extensions.gcampax.github.com" "launch-new-instance@gnome-shell-extensions.gcampax.github.com" "drive-menu@gnome-shell-extensions.gcampax.github.com" ];
          enabled-extensions = [ "native-window-placement@gnome-shell-extensions.gcampax.github.com" "unite@hardpixel.eu" ];
          welcome-dialog-last-shown-version = "46.2";
        };
    
        "org/gnome/shell/extensions/unite" = {
          app-menu-max-width = 0;
          enable-titlebar-actions = true;
          extend-left-box = false;
          greyscale-tray-icons = true;
          hide-activities-button = "auto";
          hide-app-menu-icon = true;
          hide-window-titlebars = "both";
          notifications-position = "center";
          reduce-panel-spacing = false;
          restrict-to-primary-screen = true;
          show-appmenu-button = false;
          show-desktop-name = false;
          show-window-title = "maximized";
          use-activities-text = true;
        };
    
        "org/gnome/shell/world-clocks" = {
          locations = [];
        };
    
        "org/gtk/settings/file-chooser" = {
          date-format = "regular";
          location-mode = "path-bar";
          show-hidden = false;
          show-size-column = true;
          show-type-column = true;
          sidebar-width = 157;
          sort-column = "name";
          sort-directories-first = false;
          sort-order = "ascending";
          type-format = "category";
          window-position = mkTuple [ 678 549 ];
          window-size = mkTuple [ 1203 902 ];
        };
    
      };
    }

---
+++ dconf.nix
@@ -0,0 +1,124 @@
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+{ lib, ... }:
+
+with lib.hm.gvariant;
+
+{
+  dconf.settings = {
+    "org/gnome/Console" = {
+      last-window-maximised = false;
+      last-window-size = mkTuple [ 1364 480 ];
+    };
+
+    "org/gnome/control-center" = {
+      last-panel = "display";
+      window-state = mkTuple [ 980 640 false ];
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "Utilities" "YaST" "Pardus" ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Pardus" = {
+      categories = [ "X-Pardus-Apps" ];
+      name = "X-Pardus-Apps.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
+      categories = [ "X-GNOME-Utilities" ];
+      name = "X-GNOME-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = [ "X-SuSE-YaST" ];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "us+intl" ]) ];
+      xkb-options = [ "terminate:ctrl_alt_bksp" ];
+    };
+
+    "org/gnome/desktop/notifications" = {
+      application-children = [ "gnome-power-panel" "org-gnome-console" ];
+    };
+
+    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+      application-id = "gnome-power-panel.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-console" = {
+      application-id = "org.gnome.Console.desktop";
+    };
+
+    "org/gnome/epiphany" = {
+      ask-for-default = false;
+    };
+
+    "org/gnome/epiphany/state" = {
+      is-maximized = false;
+      window-size = mkTuple [ 1024 768 ];
+    };
+
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [ 890 550 ];
+    };
+
+    "org/gnome/shell" = {
+      disabled-extensions = [ "auto-move-windows@gnome-shell-extensions.gcampax.github.com" "launch-new-instance@gnome-shell-extensions.gcampax.github.com" "drive-menu@gnome-shell-extensions.gcampax.github.com" ];
+      enabled-extensions = [ "native-window-placement@gnome-shell-extensions.gcampax.github.com" "unite@hardpixel.eu" ];
+      welcome-dialog-last-shown-version = "46.2";
+    };
+
+    "org/gnome/shell/extensions/unite" = {
+      app-menu-max-width = 0;
+      enable-titlebar-actions = true;
+      extend-left-box = false;
+      greyscale-tray-icons = true;
+      hide-activities-button = "auto";
+      hide-app-menu-icon = true;
+      hide-window-titlebars = "both";
+      notifications-position = "center";
+      reduce-panel-spacing = false;
+      restrict-to-primary-screen = true;
+      show-appmenu-button = false;
+      show-desktop-name = false;
+      show-window-title = "maximized";
+      use-activities-text = true;
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = [];
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = false;
+      show-size-column = true;
+      show-type-column = true;
+      sidebar-width = 157;
+      sort-column = "name";
+      sort-directories-first = false;
+      sort-order = "ascending";
+      type-format = "category";
+      window-position = mkTuple [ 678 549 ];
+      window-size = mkTuple [ 1203 902 ];
+    };
+
+  };
+}
