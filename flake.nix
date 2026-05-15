f2412022551063663e5f4635a83a2b561b6d36d2
Hernies <hernancalvoaguiar.2@gmail.com>
2026-05-15 21:33:32 +0200

    Refactor flake for kyuden

--- flake.nix
+++ flake.nix
@@ -1,70 +1,21 @@
 {
+  description = "Hernies NixOS systems";
 
- description = "my first flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
 
- inputs = {
-   nixpkgs.url = "nixpkgs/nixos-25.05";
-   home-manager.url = "github:nix-community/home-manager/release-25.05";
-   home-manager.inputs.nixpkgs.follows = "nixpkgs";
- };
-
- outputs = {self, nixpkgs, home-manager,  ...}:
-   let 
-     # variables that are used in the flake's modules
-     lib = nixpkgs.lib;
-     system = "x86_64-linux";
-     pkgs = nixpkgs.legacyPackages.${system};
-     username = "hernies";
-     gitArgs = {
-       name = "Hernies";
-       mail = "hernancalvoaguiar.2@gmail.com";
-     };
-     # -- future variables or var groups here -- #
-   
-   in {
-     nixosConfigurations = {
-       #nixos is my hostname
-       nixos = lib.nixosSystem {
-         inherit system;
-         modules = [ 
-          ./configuration.nix 
-          ./wireguard.nix
-         ];
-         specialArgs = {
-           inherit username;
-           # -- future inheritance of variables or var groups here -- #       
-         };
-       };
-     };
- packages = {
- nbfc = pkgs.stdenv.mkDerivation{
-  name = "nbfc-linux";
-          version = "0.1.15";
-
-          src = nixpkgs.lib.cleanSource ./.;
-
-          nativeBuildInputs = with pkgs; [
-            autoreconfHook
-          ];
-          configureFlags = [
-            "--prefix=${placeholder "out"}"
-            "--sysconfdir=${placeholder "out"}/etc"
-            "--bindir=${placeholder "out"}/bin"
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+        kyuden = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/kyuden/configuration.nix
           ];
         };
-};
-   homeConfigurations = {
-       hernies = home-manager.lib.homeManagerConfiguration {
-       inherit pkgs;
-       modules = [ ./home.nix ];
-       extraSpecialArgs = {
-         inherit username;
-         inherit gitArgs;
-         inherit lib;
-         # -- future inheritance of variables or var groups here -- #
-       };
-     };
-   };
- };
-
+      };
+    };
 }
